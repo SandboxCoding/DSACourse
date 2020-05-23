15200f8aa44a461ff12fe3d31d1c4c4c804884d5
Abiel Queiroz Machioni <60728044+SandboxCoding@users.noreply.github.com>
2020-05-23 10:33:33 -0300

    Mistakes were made. Forgot that variables in python start through number 0 instead of 1.

--- Test Stuff/If,else,elif test.py
+++ Test Stuff/If,else,elif test.py
@@ -11,14 +11,13 @@ else:
 #Trying some random things
 month = int(input("Enter a month in numeric form from 1-12: "))
 
-if month in list(range(1, 3)):
+if month in list(range(1, 4)):
     print("It's the first quarter of the month.")
-elif month in list(range(4, 6)):
+elif month in list(range(4, 7)):
     print("It's the second quarter of the month.")
-elif month in list(range(6, 9)):
+elif month in list(range(7, 10)):
     print("It's the third quarter of the month.")
-elif month in list(range(10, 12)):
+elif month in list(range(10, 13)):
     print("It's the fourth quarter of the month.")
 else:
     print("The number", month, "does not correspond to a valid month.")
-
