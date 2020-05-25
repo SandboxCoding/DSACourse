59fca8ad7317f7fe2819992884904bd6509a2562
Abiel Queiroz Machioni <60728044+SandboxCoding@users.noreply.github.com>
2020-05-25 13:16:32 -0300

    Alternate solution

--- Test Stuff/Loops.py
+++ Test Stuff/Loops.py
@@ -2,3 +2,16 @@ abc = {0:'a',1:'b',2:'c',3:'d',4:'e',5:'f',6:'g',7:'h',8:'i',9:'j',10:'k',11:'l'
 name = [abc[k] for k in [0,1,8,4,11]]
 convert = ''.join([str(elem) for elem in name])
 print(convert.capitalize())
+
+#Alternate solution from my cousin.
+
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+name = ''
+abiel_list = ['a','b','i','e','l']
+
+
+for i in range(len(abiel_list)):
+    for item in abc:
+        if item in abiel_list[i]:
+            name = name + item
+            print(name)
