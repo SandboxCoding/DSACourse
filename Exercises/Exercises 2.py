b07b2cf384eefd1bdd5e0408759d8b86da99dc2b
Abiel Queiroz Machioni <60728044+SandboxCoding@users.noreply.github.com>
2020-05-26 15:34:03 -0300

    Exercises done from 3 to 7

--- Exercises/Exercises 2.py
+++ Exercises/Exercises 2.py
@@ -21,18 +21,50 @@ for i in fruits:
 # Exercício 3 - Crie uma tupla de 4 elementos, multiplique cada elemento da tupla por 2 e guarde os resultados em uma
 # lista
 
-
+elements = (6, 8, 10, 12)
+result = []
+for i in elements:
+    mult = i * 2
+    result.append(mult)
+    print(result)
 
 # Exercício 4 - Crie uma sequência de números pares entre 100 e 150 e imprima na tela
+
+for i in range(100, 151, 2):
+    print(i)
 
 # Exercício 5 - Crie uma variável chamada temperatura e atribua o valor 40. Enquanto temperatura for maior que 35,
 # imprima as temperaturas na tela
 
+temperatura = 40
+while temperatura > 35:
+    print(temperatura)
+    temperatura = temperatura - 1
+
 # Exercício 6 - Crie uma variável chamada contador = 0. Enquanto counter for menor que 100, imprima os valores na tela,
 # mas quando for encontrado o valor 23, interrompa a execução do programa
 
+contador = 0
+
+while contador < 100:
+    if contador == 23:
+        break
+    else:
+        pass
+    print(contador)
+    contador = contador + 1
+
+
 # Exercício 7 - Crie uma lista vazia e uma variável com valor 4. Enquanto o valor da variável for menor ou igual a 20,
 # adicione à lista, apenas os valores pares e imprima a lista
+
+list = []
+value = 4
+
+while value <= 20:
+    list.append(value)
+    print(value)
+    value = value + 2
 
 # Exercício 8 - Transforme o resultado desta função range em uma lista: range(5, 45, 2)
 nums = range(5, 45, 2)
