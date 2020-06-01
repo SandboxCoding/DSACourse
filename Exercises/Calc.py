7825a42eff2101da3edcd79abfda73aabc5008e9
Abiel Queiroz Machioni <60728044+SandboxCoding@users.noreply.github.com>
2020-06-01 13:16:37 -0300

    Finished the calc exercise from Cap03.

---
+++ Exercises/Calc.py
@@ -0,0 +1,24 @@
+print('Selecione o número da operação desejada:\n')
+print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão \n' )
+
+opcao = int(input('Digite sua opção (1/2/3/4):'))
+
+if opcao == 1:
+    primeiro = float(input('Digite o primeiro número: '))
+    segundo = float(input('Digite o segundo número: '))
+    print(primeiro + segundo)
+
+elif opcao == 2:
+    primeiro = float(input('Digite o primeiro número: '))
+    segundo = float(input('Digite o segundo número: '))
+    print(primeiro - segundo)
+
+elif opcao == 3:
+    primeiro = float(input('Digite o primeiro número: '))
+    segundo = float(input('Digite o segundo número: '))
+    print(primeiro * segundo)
+
+elif opcao == 4:
+    primeiro = float(input('Digite o primeiro número: '))
+    segundo = float(input('Digite o segundo número: '))
+    print(primeiro / segundo)
